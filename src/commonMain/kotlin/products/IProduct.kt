e6ca0b0d9b738408d006aadecf5b3712efd23349
Вячеслав <v.kuznetcov@tachos.ru>
2020-01-16 17:02:30 +0400

    - init commit

---
+++ src/commonMain/kotlin/products/IProduct.kt
@@ -0,0 +1,9 @@
+package products
+/**
+ * @author Vyacheslav Kuznetsov
+ * An interface that should inherit every object that needs to be put in the basket
+ * */
+interface IProduct {
+    fun getId()
+    fun getPrice(): Double
+}
---
+++ src/commonTest/kotlin/sample/SampleTests.kt
@@ -0,0 +1,8 @@
+package sample
+
+import kotlin.test.Test
+import kotlin.test.assertTrue
+
+class SampleTests {
+
+}
